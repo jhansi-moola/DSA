eaec10b9c57d867a54f9bb12dad37dd8a248ecdd
Prathap Chandra <prathapchandra@Prathaps-MacBook-Air.local>
2022-06-26 12:21:30 +0530

    arrays - program to print elements in an array

---
+++ Arrays/print_elements_in_array.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int array[] = {1, 2, 3, 4, 5};
+    int arraySize = sizeof(array)/sizeof(array[0]);
+    for (int i = 0; i < arraySize; i++) {
+        cout << array[i] << endl;
+    }
+}
